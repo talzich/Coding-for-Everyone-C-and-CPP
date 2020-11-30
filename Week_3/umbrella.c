c7221ca7b2987abfa7e2d0b66b9296a2aa38bdb9
Tal-Zichlinsky <talzhtc@gmail.com>
2020-11-30 21:56:30 +0200

    Adding week 3

---
+++ Week_3/umbrella.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/*
+  this program will tell you wether you need an umbrella or not
+  */
+int main(void){
+  int weathter, outside;
+  printf("Is it raining? 0 for no, 1 for yes\n", );
+  scanf("%d",&weathter );
+  printf("Are you outside? 0 for no, 1 for yes\n", );
+  scanf("%d",&outside );
+  if(weathter && outside)
+    printf("looks like you need an umbrella!\n");
+  else
+    printf("Looks like you don't need an umbrella!", );
+  
+
+
+}
