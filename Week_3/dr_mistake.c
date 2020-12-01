2dbecb1954dc837810fc15b86c19cf239b59bdd3
Tal-Zichlinsky <talzhtc@gmail.com>
2020-12-01 17:57:32 +0200

    added original code

---
+++ Week_3/dr_mistake.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h> /* has  sin(), abs(), and fabs() */
+
+int main(void){
+  double interval;
+  int i;
+  for(i = 0; i <30; i++)
+  {
+    interval = i/10.0;
+    printf(“sin( %lf ) = %lf \t”, interval, abs(sin(interval)));
+  }
+  printf(“\n+++++++\n”);
+  return 0;
+}
