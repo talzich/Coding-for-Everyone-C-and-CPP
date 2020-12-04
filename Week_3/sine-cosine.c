1e7c4e6da9663fbc0a3d69dfaece016f6f56bb72
Tal-Zichlinsky <talzhtc@gmail.com>
2020-12-04 20:50:39 +0200

    Fixed comparison in while loop to be accept errors

--- Week_3/sine-cosine.c
+++ Week_3/sine-cosine.c
@@ -6,7 +6,7 @@ int main(void){
       We will increment this variable by 0.1 until it reaches 1.
       For each increment we will sine and cosine this value;
     */
-    double i = 0;
+    double i = 0.1;
 
     /*
       We will assign the value of sine and cosine of i to these variables and
@@ -14,11 +14,11 @@ int main(void){
     */
     double sineVal = 0, cosineVal =0;
 
-    while (i<1) {
-      i = i + 0.1;
+    while ((1-i) > 0.1) {
       sineVal = sin(i);
       cosineVal = cos(i);
       printf("sine of %lf is: %lf\t cosine of %lf is: %lf\n", i, sineVal, i, cosineVal);
+      i = i + 0.1;
     }
 
     return 0;
