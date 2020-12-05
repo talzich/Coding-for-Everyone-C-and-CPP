d8be1821121b02ca374892692e682a3594bbc99e
Tal-Zichlinsky <talzhtc@gmail.com>
2020-12-05 10:43:03 +0200

    Fixed bug in scanf

--- Week_3/valentines_loop.c
+++ Week_3/valentines_loop.c
@@ -3,10 +3,10 @@
 int main(void){
   int love;
   printf("How deep is your love? (1-10)\n");
-  scanf("%d\n", love);
+  scanf("%d",& love);
   if(love >= 1 && love <= 10)
   {
-    printf("I love you very\n");
+    printf("I love you\n");
     while(love > 0)
     {
       printf("very\n");
