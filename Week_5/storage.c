9bb053b6238665e0412e1bd2c03f86789c506037
Tal Zichlinsky <talzhtc@gmail.com>
2020-12-26 16:45:15 +0200

    Understood the difference between local and global variables

--- Week_5/storage.c
+++ Week_5/storage.c
@@ -3,15 +3,23 @@
 extern int reps = 0;
 
 void f(void){
-    static int called = 0;
-    printf("f called %d\n", called);
+    static int called = 1;
+    int local = 123;
+    printf("f called %d, local var = %d\n", called, local);
     called++;
     reps += called;
+    local++;
 }
 
 int main(void){
 
-    
 
+    auto int i = 1;
+    const int Limit = 10;
+    for (i = 1; i < Limit; i++)
+    {
+        printf("i local = %d, reps global = %d\n", i, reps);
+        f();
+    }
     return 0;
 }
