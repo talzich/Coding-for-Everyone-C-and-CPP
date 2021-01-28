ef93b5a9918781ddc6c9d3596f67d65fc90ba10e
Tal <talzhtc@gmail.com>
2021-01-28 22:43:50 +0200

    New file

---
+++ c_for_everyone_struct/week-1/assert2.c
@@ -0,0 +1,16 @@
+/*
+	Assert2.c from week 1
+	Uses asserts within a trivial program
+*/
+
+#define NDEBUG //goes before <assert.h>
+#include <assert.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+	assert(0);
+	printf("My Program Is Running");
+	return 0;
+}
+
