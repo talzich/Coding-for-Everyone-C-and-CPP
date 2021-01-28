58f40f324645303d4b78e18c40ec1f2bebf50151
Tal <talzhtc@gmail.com>
2021-01-28 22:38:42 +0200

    New file

---
+++ c_for_everyone_struct/week-1/assert.c
@@ -0,0 +1,17 @@
+/* Assert code from week 1
+
+   Uses asserts within a trivial program
+
+*/
+
+#include <assert.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+
+	assert(0);
+	printf("My program runs\n");
+	return 0;
+
+}
