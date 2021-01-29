b1d99c81589424d2e0b098b771bfc3ccca273478
Tal <talzhtc@gmail.com>
2021-01-29 10:29:43 +0200

    Used macros

--- c_for_everyone_struct/week-2/card.c
+++ c_for_everyone_struct/week-2/card.c
@@ -5,6 +5,13 @@
 
 #include <stdio.h>
 
+#define SPADES 's'
+#define HEARTS 'h'
+#define DIAMONDS 'd'
+#define CLUBS 'c'
+
+#define KING 13
+
 typedef struct{
 	int pips; // could be 1-13
 	char suit; // h-hearts, s-spades, c-clubs, d-diamonds
@@ -19,15 +26,15 @@ int main(void){
 	for(i = 1; i <= 52; i++){
 		deck[i-1].pips = i%13;
 		if(!(i%13))
-			deck[i-1].pips = 13;
+			deck[i-1].pips = KING;
 		if(i <= 13)
-			deck[i-1].suit = 'h';
+			deck[i-1].suit = SPADES;
 		else if(i <= 26)
-			deck[i-1].suit = 's';
+			deck[i-1].suit = HEARTS;
 		else if(i <= 39)
-                        deck[i-1].suit = 'c';
+                        deck[i-1].suit = DIAMONDS;
 		else
-			deck[i-1].suit = 'd';
+			deck[i-1].suit = CLUBS;
 	}
 
 	for(i = 0; i < 52; i++){
