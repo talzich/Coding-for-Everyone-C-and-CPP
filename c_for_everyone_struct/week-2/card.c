b36da7bb6e18cdf480f87c4c9acf2a436c636609
Tal <talzhtc@gmail.com>
2021-01-29 10:23:38 +0200

    Fixed modulo bug

--- c_for_everyone_struct/week-2/card.c
+++ c_for_everyone_struct/week-2/card.c
@@ -7,7 +7,7 @@
 
 typedef struct{
 	int pips; // could be 1-13
-	char suit; h-hearts, s-spades, c-clubs, d-diamonds
+	char suit; // h-hearts, s-spades, c-clubs, d-diamonds
 }card;
 
 
@@ -16,25 +16,19 @@ int main(void){
 	card deck[52];
 	int i;
 
-	for(i = 0; i < 13; i++){
-		deck[i].pips = i+1;
-		deck[i].suit = 'h';
+	for(i = 1; i <= 52; i++){
+		deck[i-1].pips = i%13;
+		if(!(i%13))
+			deck[i-1].pips = 13;
+		if(i <= 13)
+			deck[i-1].suit = 'h';
+		else if(i <= 26)
+			deck[i-1].suit = 's';
+		else if(i <= 39)
+                        deck[i-1].suit = 'c';
+		else
+			deck[i-1].suit = 'd';
 	}
-	
-	for(i = 0; i < 13; i++){
-                deck[i].pips = i+1;
-                deck[i].suit = 's';
-        }
-
-	for(i = 0; i < 13; i++){
-                deck[i].pips = i+1;
-                deck[i].suit = 'c';
-        }	
-
-	for(i = 0; i < 13; i++){
-                deck[i].pips = i+1;
-                deck[i].suit = 'd';
-        }
 
 	for(i = 0; i < 52; i++){
 		printf("%d of %c\n", deck[i].pips, deck[i].suit);
