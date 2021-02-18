e265bb3e97697a1d7cf0a9876707da23622e13e1
Tal <talzhtc@gmail.com>
2021-02-19 01:11:48 +0200

    Clean slate

--- c_for_everyone_fundamentals/Week2_Lexical_Elements_and_Data_Types/dr_mistake_assignment.c
+++ c_for_everyone_fundamentals/Week2_Lexical_Elements_and_Data_Types/dr_mistake_assignment.c
@@ -3,8 +3,8 @@
 
 int main(void){
  double radius;
- printf(“Enter radius:\n”);
- scanf(“%lf”, &radius);
- printf(“volume is : %lf \n“  , (4*PI*radius*radius*radius)/3.0 );
+ printf("Enter radius:\n");
+ scanf("%lf", &radius);
+ printf("volume is : %lf \n"  , (4*PI*radius*radius*radius)/3.0 );
  return 0;
 }
