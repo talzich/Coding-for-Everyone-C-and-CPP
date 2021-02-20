7d5f52ce0415afdbb3006b10b28ee97493a04081
Tal <talzhtc@gmail.com>
2021-02-20 14:26:03 +0200

    Used double instead of int

--- c_for_everyone_struct/week-5/intro_to_cpp.cpp
+++ c_for_everyone_struct/week-5/intro_to_cpp.cpp
@@ -6,15 +6,18 @@ const double m_to_k = 1.609;
 // inline is not widely used thanks to compiler flag "optimize", but this one
 // is here for educational purposes
 
-inline double convert(int mi) { return mi*m_to_k; }
+inline double convert(double mi) { return mi*m_to_k; }
 
 int main (void){
-
-	int miles = 1;
+	
+	// Dr. Pohl used int as an input to mile but I checked it and thought 
+	// it would be more appropriate to use double instead
+	double miles = 1;
 	while(miles != 0){
 		cout << "Input distance in miles or 0 to terminate\n";
 		cin >> miles;
+		// endl is used to create a new line, just like '\n'
 		cout << "Distance in kilometers: " << convert(miles) << endl;
 	}
-	cout endl;
+	cout << endl;
 }
