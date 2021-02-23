1d32ac2a1ffd803a8fc5a18a614767dfc35b4036
Tal <talzhtc@gmail.com>
2021-02-24 00:34:35 +0200

    New file

---
+++ c_for_everyone_struct/week-5/demo.cpp
@@ -0,0 +1,28 @@
+// Using some of the C++ features that Dr. P 
+// Talked about
+// Author: Tal Zichlinsky
+
+#include <iostream>
+using namespace :: std;
+
+long long fact(int n){
+	long long f = 1;
+	for(int i = 1; i <= n; i++){
+		f *= i;
+	}
+	return f;
+}
+
+int main(void){
+
+	cout << "This program uses some features that are not in C89\n";
+	
+	// g++ compiler is not happy with that auto declaration
+	// Will try to fix that	
+	for(auto i = 0; i < 16; i++){
+		cout << "factorial of " << i << " is " << fact(i) << endl;
+	}
+	cout <<  "Thats all folks!" << endl;
+	return 0;
+}
+ 
