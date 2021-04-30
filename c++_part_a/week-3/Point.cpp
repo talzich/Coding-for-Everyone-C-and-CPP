a477e6def7471f074bf2ac979a2260b11b11d52f
Tal <talzhtc@gmail.com>
2021-04-30 17:28:09 +0300

    Added documentation

--- c++_part_a/week-3/Point.cpp
+++ c++_part_a/week-3/Point.cpp
@@ -4,11 +4,19 @@
 class point{
     
     double x, y;
-    
-    public:
-        // Constructor
-        point(double x = 0.0, double y = 0.0):x(x), y(y){}
+    const int tester;
 
+    public:
+        // Constructor of preferred type
+        // The reason this constructor is preferred is that the members are considered initialized and not assignment so we can assign a value to a const member
+        point(double x = 0.0, double y = 0.0):x(x), y(y), tester(10){}
+
+        // point(double x, double y, int tester){
+        //     this->x = x; 
+        //     this->y = y; 
+        //     // this->tester = tester; This line will not compile because this style of assignment is considered mutating
+        // }
+        
         // Getters & Setters
         double get_x() const { return x; }
         double get_y() const { return y; }
