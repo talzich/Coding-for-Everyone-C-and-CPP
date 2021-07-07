beb7796a8e7e81481f332cc09e5c78a22e871871
Tal <talzhtc@gmail.com>
2021-07-07 20:18:07 +0300

    Added main and more extra functionality

--- c++_part_a/week-3/List.cpp
+++ c++_part_a/week-3/List.cpp
@@ -1,6 +1,7 @@
 //
 // AUTHOR: Dr. Ira Pohl of the University of California. 
 // EDITED BY: Tal Zichlinsky
+// COMPILE WITH: clang++ -std=c++2a
 // This file will contain declarations as well as some of the definitions of the list and list_element classes
 #include <iostream>
 
@@ -14,20 +15,33 @@ class list_element{
 
 class list{
     list_element *head;
-    list_element *ptr;
+    list_element *arbitrary_ptr;
+    list_element *end_ptr;
     public:
-        list(): head(nullptr), ptr(nullptr){}
-        int get_element(){ return ptr->data; }
-        void advance(){ ptr = ptr->next; }
+        list(): head(nullptr), arbitrary_ptr(nullptr){}
+        int get_element(){ return arbitrary_ptr->data; }
+        void advance(){ arbitrary_ptr = arbitrary_ptr->next; }
         void prepend(int n)
         {
             if(head == nullptr)
             {
-                ptr = head = new list_element(n, head);
+                arbitrary_ptr = head = end_ptr = new list_element(n, head);
             }
             else
             {
                 head = new list_element(n, head);
+            }
+        }
+        void append(int n)
+        {
+            if(head == nullptr)
+            {
+                arbitrary_ptr = head = end_ptr = new list_element(n, NULL);
+            }
+            else
+            {
+                end_ptr->next = new list_element(n, NULL);
+                end_ptr = end_ptr->next;
             }
         }
         void print()
@@ -35,9 +49,23 @@ class list{
             list_element *curr = head;
             while(curr != nullptr)
             {
-                std::cout << curr->data << ", ";
+                std::cout << curr->data << " -> ";
                 curr = curr->next;
             }
             std::cout << "END" << std::endl;
         }
 };// END OF LIST
+
+int main()
+{
+    list l{};
+    for(int i = 1; i <= 5; ++i)
+    {
+        l.append(i);
+    }
+    for(int i = 6; i <= 10; ++i)
+    {
+        l.prepend(i);
+    }
+    l.print();
+}
